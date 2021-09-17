9c76d7b54e22fad831fcb5b97c852ca1e704d329
Mor Codou GUEYE <morcodou@gmail.com>
2021-09-17 00:24:47 -0400

    Adding post service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './services/prisma.service';
+import { PostsService } from './services/posts.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, PostsService],
 })
 export class AppModule {}
---
+++ src/services/posts.service.ts
@@ -0,0 +1,58 @@
+import { Post } from '.prisma/client';
+import { Injectable } from '@nestjs/common';
+import { NewPost, UpdatePost } from 'src/graphql';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class PostsService {
+
+    constructor(private prisma: PrismaService) {
+    }
+
+    // Get a single post
+    async post(id: string): Promise<Post | null> {
+        return this.prisma.post.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+    }
+
+    // Get all posts
+    async posts(): Promise<Post[]> {
+        return this.prisma.post.findMany({});
+    }
+
+    // Create a post
+    async createPost(input: NewPost): Promise<Post> {
+        return this.prisma.post.create({
+            data: input
+        });
+    }
+
+    // update a post
+    async updatePost(params: UpdatePost): Promise<Post> {
+        const { id, title, content, published } = params;
+
+        return this.prisma.post.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                ...(published && { published }),
+                ...(title && { title }),
+                ...(content && { content }),
+            }
+        });
+    }
+
+
+    // delete a post
+    async delete(id: string): Promise<Post> {
+        return this.prisma.post.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+    }
+}
